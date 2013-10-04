df88b5ff9dba7af490d10fe219ef2bcd03afeb5f
Chris Lucas <christopher.lucas@gmail.com>
2013-10-04 14:23:36 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Staffsauce::Application.config.secret_key_base = '5902c49eae7baf90d88da027ea64da1afe6273a072cd07e49701efd45d21265389e2867ba38f27a3525d7db9f9eedfe4b334ca692285f56473b291feb18514d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Staffsauce::Application.config.session_store :cookie_store, key: '_staffsauce_session'
